2f0819312b2e5e53da5db9b5cc184f58068b8ec1
ekadesi48 <62577646+ekadesi48@users.noreply.github.com>
2021-12-13 08:37:52 -0800

    Add files via upload

--- db/eplusgo_karyawan.sql
+++ db/eplusgo_karyawan.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Oct 15, 2020 at 10:38 AM
--- Server version: 5.7.30-0ubuntu0.18.04.1
--- PHP Version: 5.6.40-30+ubuntu18.04.1+deb.sury.org+1
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2021 at 08:41 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -33,6 +34,16 @@ CREATE TABLE `cuti` (
   `jumlah` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `cuti`
+--
+
+INSERT INTO `cuti` (`id`, `karyawan_id`, `tanggal_cuti`, `jumlah`) VALUES
+(1, 1, '2021-11-01', 4),
+(2, 2, '2021-11-03', 3),
+(3, 3, '2021-10-11', 4),
+(4, 2, '2021-10-18', 3);
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +60,15 @@ CREATE TABLE `golongan` (
   `tunjangan_makan` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `golongan`
+--
+
+INSERT INTO `golongan` (`id`, `nama_golongan`, `gaji_pokok`, `tunjangan_istri`, `tunjangan_anak`, `tunjangan_transport`, `tunjangan_makan`) VALUES
+(1, 'Madya', 14000000, 5000000, 4000000, 1000000, 2000000),
+(2, 'Utama ', 20000000, 9000000, 4000000, 1000000, 3000000),
+(3, 'Teknisi', 50000000, 6000000, 4000000, 5000000, 6000000);
+
 -- --------------------------------------------------------
 
 --
@@ -57,19 +77,27 @@ CREATE TABLE `golongan` (
 
 CREATE TABLE `karyawan` (
   `id` int(11) UNSIGNED NOT NULL,
-  `nip` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `nik` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `nama` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `jenis_kelamin` enum('pria','wanita') COLLATE utf8_unicode_ci NOT NULL,
   `tempat_lahir` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `tanggal_lahir` date NOT NULL,
   `telpon` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `agama` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `status_nikah` enum('belum nikah','nikah') COLLATE utf8_unicode_ci NOT NULL,
   `alamat` text COLLATE utf8_unicode_ci NOT NULL,
-  `golongan_id` int(11) UNSIGNED NOT NULL,
-  `foto` varchar(150) COLLATE utf8_unicode_ci NOT NULL
+  `golongan_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`id`, `nik`, `nama`, `jenis_kelamin`, `tempat_lahir`, `telpon`, `agama`, `alamat`, `golongan_id`) VALUES
+(1, '123', 'Dewa Dayana', 'pria', 'malang', '089878978', 'islam', 'Jakarta', 1),
+(2, '124', 'Arsaka', 'pria', 'Madiun', '0809878367', 'kristen', 'Kalbar', 1),
+(3, '1234', 'arsen', 'pria', 'surabaya', '0889770868', 'hindu', 'kota batu', 3),
+(4, '125', 'Rasya', 'pria', 'Kupang', '08776562423', 'islam', 'Bali', 2),
+(5, '126', 'Rafli', 'pria', 'surabaya', '0809878367', 'kristen', 'Bali', 2),
+(6, '127', 'Reno', 'pria', 'Manila', '0876656565', 'islam', 'Derawan', 3);
 
 -- --------------------------------------------------------
 
@@ -100,6 +128,27 @@ CREATE TABLE `penggajian` (
   `potongan` int(11) NOT NULL,
   `total_gaji_diterima` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(150) DEFAULT NULL,
+  `last_name` varchar(150) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`) VALUES
+(1, 'Eka', 'Desi', 'ekadesi@gmail.com', '$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq');
 
 --
 -- Indexes for dumped tables
@@ -140,6 +189,12 @@ ALTER TABLE `penggajian`
   ADD KEY `penjualan_pelanggan_id` (`karyawan_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -147,27 +202,38 @@ ALTER TABLE `penggajian`
 -- AUTO_INCREMENT for table `cuti`
 --
 ALTER TABLE `cuti`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `golongan`
 --
 ALTER TABLE `golongan`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `lembur`
 --
 ALTER TABLE `lembur`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `penggajian`
 --
 ALTER TABLE `penggajian`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- Constraints for dumped tables
 --
@@ -195,6 +261,7 @@ ALTER TABLE `lembur`
 --
 ALTER TABLE `penggajian`
   ADD CONSTRAINT `penggajian_karyawan_id` FOREIGN KEY (`karyawan_id`) REFERENCES `karyawan` (`id`) ON DELETE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
