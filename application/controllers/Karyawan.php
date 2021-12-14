6243b61802e46bd2512832e1569b746495d84255
ekadesi48 <62577646+ekadesi48@users.noreply.github.com>
2021-12-13 23:38:39 -0800

    tugas2

---
+++ application/controllers/Karyawan.php
@@ -0,0 +1,137 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+require "vendor\autoload.php";
+
+use Restserver\Libraries\REST_Controller;
+use \Firebase\JWT\JWT;
+
+class Karyawan extends REST_Controller
+{
+
+    function __construct($config = 'rest')
+    {
+        parent::__construct($config);
+    }
+
+    //untuk Menampilkan keseluruhan data
+    public function index_get()
+    {
+        $authHeader = $this->input->get_request_header('Authorization');
+        $arr = explode(" ", $authHeader);
+        $jwt = isset($arr[1]) ? $arr[1] : "";
+        $secretkey = base64_encode("gampang");
+
+        if ($jwt) {
+            try {
+                $decoded = JWT::decode($jwt, $secretkey, array('HS256'));
+
+                $id = $this->get('id');
+                if ($id == '') {
+                    $data = $this->db->get('karyawan')->result();
+                } else {
+                    $this->db->where('id', $id);
+                    $data = $this->db->get('karyawan')->result();
+                }
+                $result = [
+                    "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                    "code" => 200,
+                    "message" => "Response successfully",
+                    "data" => $data
+                ];
+                $this->response($result, 200);
+            } catch (Exception $e) {
+
+                $result = [
+                    "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                    "code" => 401,
+                    "message" => "Access denied",
+                    "data" => null
+                ];
+                $this->response($result, 401);
+            }
+        } else {
+            $result = [
+                "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                "code" => 401,
+                "message" => "Access denied",
+                "data" => null
+            ];
+            $this->response($result, 401);
+        }
+    }
+
+
+    // untuk menambahkan data baru
+    public function index_post()
+    {
+        $data = array(
+            'id'       => $this->post('id'),
+            'nik'              => $this->post('nik'),
+            'nama'       => $this->post('nama'),
+            'jenis_kelamin'       => $this->post('jenis_kelamin'),
+            'tempat_lahir'       => $this->post('tempat_lahir'),
+            'telpon'       => $this->post('telpon'),
+            'agama'       => $this->post('agama'),
+            'alamat'       => $this->post('alamat'),
+            'golongan_id'       => $this->post('golongan_id')
+        );
+        $insert = $this->db->insert('karyawan', $data);
+        if ($insert) {
+            $result = [
+                "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                "code" => 201,
+                "message" => "Data has successfully added",
+                "data" => $data
+            ];
+            $this->response($result, 201);
+        } else {
+            $result = [
+                "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                "code" => 502,
+                "message" => "Failed adding data",
+                "data" => null
+            ];
+            $this->response($result, 502);
+        }
+    }
+
+    //untuk mengubah data
+    public function index_put()
+    {
+        $id = $this->put('id');
+        $data = array(
+            'id'       => $this->put('id'),
+            'nik'              => $this->put('nik'),
+            'nama'       => $this->put('nama'),
+            'jenis_kelamin'       => $this->put('jenis_kelamin'),
+            'tempat_lahir'       => $this->put('tempat_lahir'),
+            'telpon'       => $this->put('telpon'),
+            'agama'       => $this->put('agama'),
+            'alamat'       => $this->put('alamat'),
+            'golongan_id'       => $this->put('golongan_id')
+        );
+        $this->db->where('id', $id);
+        $update = $this->db->update('karyawan', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    //untuk Menghapus Data
+    public function index_delete()
+    {
+        $id = $this->delete('id');
+        $this->db->where('id', $id);
+        $delete = $this->db->delete('karyawan');
+        if ($delete) {
+            $this->response(array('status' => 'success'), 201);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+}
